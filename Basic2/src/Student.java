61fa5ba9a1ab053829ca81c15ba6a30648e31bec
Raofin Jahan Shajin <raofinjahanshajin@gmail.com>
2025-07-20 20:22:46 +0600

    Add files via upload
    ✅ Classes & Objects
    
    Class: A blueprint (template) describing the structure and behavior of objects.
    Object: An instance of a class; represents an entity with state and behavior.

---
+++ Basic2/src/Student.java
@@ -0,0 +1,9 @@
+class Student {
+    String name;
+    int roll;
+
+    void displayInfo() {
+        System.out.println("Student Name: " + name);
+        System.out.println("Roll Number: " + roll);
+    }
+}
